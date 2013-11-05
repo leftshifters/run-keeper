5d693c2d5c2229510af4b581c6464ab698f2630c
Sudhanshu Raheja <sudhanshu@vxtindia.com>
2013-11-06 00:15:21 +0530

    Got activities via test

--- examples/index.js
+++ examples/index.js
@@ -8,24 +8,27 @@ var port = 3456;
 var run = new runkeeper();
 run.options.client_id = "";
 run.options.client_secret = "";
-run.options.redirect_uri = "http://localhost:" + port;
+run.options.redirect_uri = "http://localhost:" + port + "/runkeeper";
+
+var authorization_code = "";
 
 app.get('/login', function(req, res) {
 	res.redirect(run.getAuthorizationURL());
 });
 app.get('/runkeeper', function(req, res) {
-	var authorization_code = req.query.code;
-	run.getAccessToken(authorization_code, function(err, data) {
-		if(err) console.log(err);
-		else {
-			run.getFitnessActivities(data, function(err, data) {
-				if(err) console.log(err);
-				else console.log(data);
-			});
-		}
-	});
+	authorization_code = req.query.code;
+	run.getAccessToken(authorization_code, handleAccessToken);
 });
 
+var handleAccessToken = function(err, data) {
+	if(err) console.log(err);
+	else run.getFitnessActivities(data, handleFitnessActivities);
+};
+
+var handleFitnessActivities = function(err, data) {
+	if(err) console.log(err);
+	else console.log(data);
+};
 
 http.createServer(app).listen(port, function(){
   console.log('Express server listening on port ' + port);
